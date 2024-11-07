e783c2892bea22db0c0439381e3a74ff24a97a7b
Brack <clement.strappazzon.marques@outlook.fr>
2024-11-07 20:43:24 +0100

    dynamic group

---
+++ Assets/Scripts/GameContent/Entities/OnFieldEntities/DynamicBuildingList.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+
+namespace GameContent.Entities.OnFieldEntities
+{
+    public sealed class ConveyorGroup
+    {
+        #region properties
+        
+        public List<DynamicBuilding> Dynamic
+        
+        #endregion
+        
+        #region constructors
+
+        public ConveyorGroup(List<> )
+        {
+            
+        }
+        
+        #endregion
+        
+        #region methodes
+        
+        
+        
+        #endregion
+    }
+}
