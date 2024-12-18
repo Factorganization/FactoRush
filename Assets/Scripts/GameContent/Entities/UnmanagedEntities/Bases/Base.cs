2d664ce712c634c3f0693bf3d83cb26281ecb7a7
Asemerald <63583036+Asemerald@users.noreply.github.com>
2024-12-18 15:52:46 +0100

    j'ai rattrapé mon taf quasiment

---
+++ Assets/Scripts/GameContent/Entities/UnmanagedEntities/Bases/Base.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+namespace GameContent.Entities.UnmanagedEntities
+{
+    public class Base : MonoBehaviour
+    {
+        public float Health;
+        public float Damage;
+        public bool IsDead;
+        
+        private void Awake()
+        {
+            OnAwake();
+        }
+        
+        protected virtual void OnAwake()
+        {
+            Health = 100;
+            Damage = 10;
+            IsDead = false;
+        }
+        
+        public virtual void TakeDamage(float damage)
+        {
+            Health -= damage;
+            if (Health <= 0)
+            {
+                Die();
+            }
+        }
+        
+        protected virtual void Die()
+        {
+            IsDead = true;
+        }
+        
+        
+    }
+}
--- Assets/Scripts/GameContent/Entities/UnmanagedEntities/UnitsManager.cs
+++ Assets/Scripts/GameContent/Entities/UnmanagedEntities/UnitsManager.cs
@@ -1,3 +1,4 @@
+using System;
 using GameContent.Entities.OnFieldEntities.Buildings;
 using GameContent.Entities.UnmanagedEntities;
 using UnityEngine;
@@ -8,11 +9,40 @@ public class UnitsManager : MonoBehaviour
     #region Fields
     
     [SerializeField] private Unit[] units;
+    [SerializeField] private GameObject unitPrefab;
     
     [Header("Factory")]
-    [SerializeField] private FactoryBuilding allyFactory;
-    [SerializeField] private FactoryBuilding enemyFactory;
+    [SerializeField] private AllyBase allyBase;
+    [SerializeField] private EnemyBase enemyBase;
     
     #endregion
 
+    private void Start()
+    {
+        InitialCheckup();
+    }
+
+    private void InitialCheckup()
+    {
+        if (allyBase == null)
+        {
+            allyBase = AllyBase.Instance;
+            Debug.LogWarning("Ally base is not assigned in the inspector. Assigning it to the instance of AllyBase");
+        }
+        if (enemyBase == null)
+        {
+            enemyBase = EnemyBase.Instance;
+            Debug.LogWarning("Enemy base is not assigned in the inspector. Assigning it to the instance of EnemyBase");
+        }
+        if (unitPrefab == null)
+        {
+            Debug.LogError("Unit prefab is not assigned in the inspector. Please assign it.");
+        }
+        
+        if (allyBase == null || enemyBase == null || unitPrefab == null)
+        {
+            Debug.LogError("UnitsManager cannot function without the required components. Please assign them in the inspector.");
+            return;
+        }
+    }
 }
