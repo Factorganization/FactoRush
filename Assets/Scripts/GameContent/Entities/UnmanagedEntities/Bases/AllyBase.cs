0cbad41d74f472ae6e3efed464757b6c737faefd
Asemerald <63583036+Asemerald@users.noreply.github.com>
2025-01-14 22:49:08 +0100

    GAMELOOP ?

--- Assets/Scripts/GameContent/Entities/UnmanagedEntities/Bases/AllyBase.cs
+++ Assets/Scripts/GameContent/Entities/UnmanagedEntities/Bases/AllyBase.cs
@@ -22,6 +22,7 @@ namespace GameContent.Entities.UnmanagedEntities
         protected override void Die()
         {
             Debug.Log("Ally is dead");
+            GameManager.Instance.AllyWin();
         }
 
         public override void TakeDamage(float damage)
