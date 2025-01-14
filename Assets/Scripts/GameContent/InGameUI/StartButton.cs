6f9c5ce989eeb8017a72413e33d1affb4e8a5f40
Brack <clement.strappazzon.marques@outlook.fr>
2025-01-14 22:45:59 +0100

    button

---
+++ Assets/Scripts/GameContent/InGameUI/StartButton.cs
@@ -0,0 +1,20 @@
+﻿using GameContent.GridManagement;
+using UnityEngine;
+
+namespace GameContent.InGameUI
+{
+    public class StartButton : MonoBehaviour
+    {
+        #region methodes
+
+        public void SetStartButton()
+        {
+            if (GridManager.Manager.CurrentLockMode is not GridLockMode.Unlocked)
+                return;
+
+            GameManager.Instance.CanStart = true;
+        }
+
+        #endregion
+    }
+}
