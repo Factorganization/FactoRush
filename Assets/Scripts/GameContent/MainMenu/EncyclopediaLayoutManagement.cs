f22f43ad5a3ec6f6001bdd26c885382ff0ab4b12
Brack <clement.strappazzon.marques@outlook.fr>
2025-01-15 02:45:16 +0100

    final build

---
+++ Assets/Scripts/GameContent/MainMenu/EncyclopediaLayoutManagement.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+namespace GameContent.MainMenu
+{
+    public class EncyclopediaLayoutManagement : MonoBehaviour
+    {
+        #region methodes
+
+        public void OpenPanel(int i)
+        {
+            vLayout.SetActive(false);
+            panels[i].SetActive(true);
+        }
+
+        public void ClosePanel()
+        {
+            foreach (var panel in panels)
+            {
+                panel.SetActive(false);
+            }
+            vLayout.SetActive(true);
+        }
+
+        #endregion
+        
+        #region fields
+
+        [SerializeField] private GameObject vLayout;
+        
+        [SerializeField] private GameObject[] panels;
+
+        #endregion
+    }
+}
